b68f11413656485321a3be68d090b3e4c238b195
Nega7368 <nega7368@gmail.com>
2022-08-18 19:04:39 +0530

    i love my anna

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,30 @@
+package org.computer;
+
+public  class Desktop implements Hardware,Software {
+@Override
+public void hardwareResource() {
+	System.out.println(" Hardware resource is manufactured");
+}
+@Override
+	public void softwareResource() {
+	System.out.println(" Software Resource is testing");
+		
+	}	
+	private void desktopModel() {
+		System.out.println("Desktop model is Windows 7");
+	}
+	public static void main(String[] args) {
+		Desktop a = new Desktop ();
+		a.desktopModel();
+		a.hardwareResource();
+		a.softwareResource();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/computer/Hardware.java
@@ -0,0 +1,7 @@
+package org.computer;
+
+public interface Hardware {
+void hardwareResource();
+	
+	
+}
